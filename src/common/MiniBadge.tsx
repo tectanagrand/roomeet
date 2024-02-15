c91a8066263cab8a57d4fdef870ca74e0c919dd9
tectanagrand <rtektano@gmail.com>
2024-02-15 13:26:32 +0700

    fix : size minibadge room card

--- src/common/MiniBadge.tsx
+++ src/common/MiniBadge.tsx
@@ -9,9 +9,7 @@ export default function MiniBadge({
 }) {
   return (
     <div className={clsx("p-2 rounded-full", color)}>
-      <p className="my-0 py-0 text-[0.55em]/[0.5em] md:text-xs text-neutral-50">
-        {text}
-      </p>
+      <p className="my-0 py-0 text-xs text-neutral-50">{text}</p>
     </div>
   );
 }
