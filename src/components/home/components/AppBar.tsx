775a1f13fad5f35bdc6f455d2a6d3e80c7f49bb1
tectanagrand <rtektano@gmail.com>
2024-02-15 13:30:49 +0700

    fix : user menu

--- src/components/home/components/AppBar.tsx
+++ src/components/home/components/AppBar.tsx
@@ -5,15 +5,25 @@ import {
   BookmarkIcon,
   PlusIcon,
 } from "@heroicons/react/24/outline";
-import { Button, Popover, Box } from "@mui/material";
+import { Button, Popover, Box, Avatar, IconButton } from "@mui/material";
 import ButtonCard from "@/common/ButtonCard";
-import { useState } from "react";
+import { BaseSyntheticEvent, useState } from "react";
+import UserMenu from "@/common/UserMenu";
 
 const AppBar = () => {
   const mobile = useMediaQuery("(max-width:480px)");
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [anchorMenu, setAnchorMenu] = useState<HTMLButtonElement | null>(null);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleUserMenu = (e: BaseSyntheticEvent) => {
+    setAnchorMenu(e.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorMenu(null);
   };
 
   const handleClose = () => {
@@ -27,11 +37,11 @@ const AppBar = () => {
           {!mobile && (
             <>
               <Button
-                className="rounded-full p-0"
+                className="rounded-full p-0 "
                 disableRipple={true}
                 onClick={handleClick}
               >
-                <Bars3Icon className="h-4 w-4" />
+                <Bars3Icon className="h-8 w-8 text-neutral-950" />
               </Button>
               <Popover
                 open={open}
@@ -50,7 +60,7 @@ const AppBar = () => {
                   <ButtonCard
                     name="Home"
                     icon={<HomeIcon className="text-neutral-100" />}
-                    link="/"
+                    link="/dashboard"
                   />
                   <ButtonCard
                     name="List Book"
@@ -60,7 +70,7 @@ const AppBar = () => {
                   <ButtonCard
                     name="New Book"
                     icon={<PlusIcon className="text-neutral-100" />}
-                    link="/book"
+                    link="dashboard/book"
                   />
                 </Box>
               </Popover>
@@ -69,7 +79,10 @@ const AppBar = () => {
           <p className="font-semibold grow md:text-center text-neutral-900">
             ROOMEET
           </p>
-          <div className="h-4 w-4 bg-neutral-600 rounded-full"></div>
+          <IconButton className="p-0" onClick={handleUserMenu}>
+            <Avatar className="h-8 w-8 bg-neutral-600 rounded-full" />
+          </IconButton>
+          <UserMenu anchorEl={anchorMenu} handleClose={handleMenuClose} />
         </div>
       </div>
     </div>
