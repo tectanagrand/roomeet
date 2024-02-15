2ac4a0a14b51e1657a32b229527ada5563224bd3
tectanagrand <rtektano@gmail.com>
2024-02-15 13:27:26 +0700

    feat : password field comp

---
+++ src/common/PasswordWithEyes.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/16/solid";
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { Control, Controller } from "react-hook-form";
+import { MouseEventHandler, SyntheticEvent, useState } from "react";
+
+interface PasswordWithEyesProps {
+  control: Control<any> | undefined;
+  label: string;
+  name: string;
+  rules?: object;
+}
+
+export const PasswordWithEyes = ({
+  control,
+  label,
+  name,
+  rules,
+}: PasswordWithEyesProps) => {
+  const [showPassword, setPwd] = useState(false);
+
+  const handleClickShowPassword = (event: SyntheticEvent<EventTarget>) =>
+    setPwd((show) => !show);
+
+  const handleMouseDownPassword =
+    (event: SyntheticEvent<EventTarget>) => () => {
+      event.preventDefault();
+    };
+  return (
+    <>
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field: { onChange, value }, fieldState: { error } }) => (
+          <FormControl fullWidth variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              {label}
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              onChange={onChange}
+              value={value}
+              error={!!error}
+              inputProps={{ autoComplete: "new-password" }}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? (
+                      <EyeIcon className="h-8 w-8" />
+                    ) : (
+                      <EyeSlashIcon className="h-8 w-8" />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label={label}
+            />
+          </FormControl>
+        )}
+      />
+    </>
+  );
+};
