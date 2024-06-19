d9c464aeda5dc9aaae5615cbe22fe20fb63f7e2e
faizbyp <faizbyp@gmail.com>
2024-06-19 14:16:29 +0700

    add: landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import { Box, Typography, Button } from "@mui/material";
+import Link from "next/link";
+
 export default function HomePage() {
-  return <div className="items-center justify-center">Landing Page</div>;
+  return <>
+    <Box sx={{minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
+      <Typography variant="h4">
+        KPN Corp
+      </Typography>
+      <Typography variant="subtitle1" sx={{color: "primary.main", textTransform: "uppercase", fontWeight: "bold"}}>Book your meeting room</Typography>
+      <Link href="/dashboard" passHref>
+        <Button variant="contained" className="btn-primary">Start Now</Button>
+      </Link>
+    </Box>
+  </>;
 }
