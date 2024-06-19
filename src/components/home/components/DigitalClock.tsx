a83f262e49fd684e69ad9adb632d05eb20ed96ec
faizbyp <faizbyp@gmail.com>
2024-06-19 17:13:50 +0700

    add: skeleton on clock

--- src/components/home/components/DigitalClock.tsx
+++ src/components/home/components/DigitalClock.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Skeleton } from "@mui/material";
 import * as date from "date-fns";
 import { useState, useEffect } from "react";
 
@@ -30,16 +31,28 @@ export default function DigitalClock() {
     <div className="flex flex-col my-2">
       <div className="px-4 md:px-10 flex items-center">
         <div className="rounded-full w-4 h-4 mx-2 bg-neutral-50"></div>
-        <p className="">{dateNow}</p>
+        {dateNow ? (
+          <p className="">{dateNow}</p>
+        ) : (
+          <Skeleton variant="rounded" width={216} height={48} className="bg-neutral-600" />
+        )}
       </div>
-      <div className="flex gap-4 w-full justify-center ">
-        <p className="text-[64pt] text-center text-neutral-50 w-32 my-0">
-          {hour}
-        </p>
+      <div className="flex gap-4 w-full justify-center">
+        {hour ? (
+          <p className="text-[64pt] text-center text-neutral-50 w-32 my-0">
+            {hour}
+          </p>
+        ) : (
+          <Skeleton variant="rounded" width={96} height={96} className="bg-neutral-600 my-8" />
+        )}
         <p className="text-[64pt] text-center text-neutral-50 my-0">:</p>
-        <p className="text-[64pt] text-center text-neutral-50 w-32 my-0">
-          {minute}
-        </p>
+        {minute ? (
+          <p className="text-[64pt] text-center text-neutral-50 w-32 my-0">
+            {minute}
+          </p>
+        ) : (
+          <Skeleton variant="rounded" width={96} height={96} className="bg-neutral-600 my-8" />
+        )}
         {/* <p className="text-[64pt] text-center text-neutral-50 my-0">:</p>
         <p className="text-[64pt] text-center text-neutral-50 w-32 my-0">
           {second}
